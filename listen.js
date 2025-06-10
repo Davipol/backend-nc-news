bc8bf266305fec50358ce20f69cce5f85110206b
Davipol <davi-liz@hotmail.it>
2025-06-10 11:54:22 +0100

    Setup production environment, update connection pool, update package.json

--- listen.js
+++ listen.js
@@ -1,9 +1,10 @@
 const app = require("./app.js");
+const { PORT = 9090 } = process.env;
 
-app.listen(9090, (err) => {
+app.listen(PORT, (err) => {
   if (err) {
     console.log(err);
   } else {
-    console.log("Listening on 9090");
+    console.log(`Listening on ${PORT}...`);
   }
 });
